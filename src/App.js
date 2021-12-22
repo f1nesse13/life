c7c044d4fe4bc356120e473b60bd509bafcf5f41
f1nesse13 <philly.teg@gmail.com>
2021-12-22 02:36:45 -0500

    move row,col state to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { useState } from 'react'
 import Game from './Game/Game'
+import Navigation from './Navigation/Navigation'
 
-const App = () => (
-  <div>
-    <Game />
-  </div>
-)
+const App = () => {
+  const [NUM_ROWS, setNumRows] = useState(null)
+  const [NUM_COL, setNumCol] = useState(null)
+  const [activeGame, setActiveGame] = useState(false)
+  return (
+    <>
+      <Navigation
+        NUM_ROWS={NUM_ROWS}
+        NUM_COL={NUM_COL}
+        activeGame={activeGame}
+        setActiveGame={setActiveGame}
+        setNumRows={setNumRows}
+        setNumCol={setNumCol}
+      />
+      <Game NUM_ROWS={NUM_ROWS} NUM_COL={NUM_COL} activeGame={activeGame} />
+    </>
+  )
+}
 
 export default App
--- src/Game/Game.jsx
+++ src/Game/Game.jsx
@@ -1,24 +1,13 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import Window from './Window'
-import Navigation from './Navigation/Navigation'
 
-const NUM_ROWS = 500
-const NUM_COL = 500
-const Game = (props) => {
-  const [activeGame, setActiveGame] = useState(false)
-  const [boardState, setBoardState] = useState([[]])
-  let NUM_ROWS, NUM_COL
-  return (
-    <div>
-      <Navigation />
-      <Window
-        setBoardState={setBoardState}
-        NUM_ROWS={NUM_ROWS}
-        NUM_COL={NUM_COL}
-        activeGame={activeGame}
-      />
-    </div>
-  )
-}
+const Game = ({ NUM_ROWS, NUM_COL, activeGame }) => (
+  <Window
+    setBoardState={setBoardState}
+    NUM_ROWS={NUM_ROWS}
+    NUM_COL={NUM_COL}
+    activeGame={activeGame}
+  />
+)
 
 export default Game
