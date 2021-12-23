a952a6280e6c0d285ca054a9cece9a6372bde2ea
f1nesse13 <philly.teg@gmail.com>
2021-12-23 17:01:00 -0500

    component for loading icon (text only atm)

---
+++ src/Game/LoadingIcon.jsx
@@ -0,0 +1,14 @@
+const LoadingIcon = () => (
+  <div
+    style={{
+      width: '100vw',
+      height: '100vh',
+      textAlign: 'center',
+      padding: 'auto',
+    }}
+  >
+    LOADING...
+  </div>
+)
+
+export default LoadingIcon
