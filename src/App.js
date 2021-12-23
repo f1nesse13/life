99e5d660e2d92e626d40810435655a8526ce67ee
f1nesse13 <philly.teg@gmail.com>
2021-12-23 16:56:05 -0500

    move app state into game - add iteration speed option and game start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,5 @@
-import { useState } from 'react'
 import Game from './Game/Game'
-import Navigation from './Navigation/Navigation'
 
-const App = () => {
-  const [NUM_ROWS, setNumRows] = useState(null)
-  const [NUM_COL, setNumCol] = useState(null)
-  const [activeGame, setActiveGame] = useState(false)
-  return (
-    <>
-      <Navigation
-        NUM_ROWS={NUM_ROWS}
-        NUM_COL={NUM_COL}
-        activeGame={activeGame}
-        setActiveGame={setActiveGame}
-        setNumRows={setNumRows}
-        setNumCol={setNumCol}
-      />
-      <Game NUM_ROWS={NUM_ROWS} NUM_COL={NUM_COL} activeGame={activeGame} />
-    </>
-  )
-}
+const App = () => <Game />
 
 export default App
--- src/Game/Game.jsx
+++ src/Game/Game.jsx
@@ -1,13 +1,44 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Window from './Window'
+import Navigation from '../Navigation/Navigation'
 
-const Game = ({ NUM_ROWS, NUM_COL, activeGame }) => (
-  <Window
-    setBoardState={setBoardState}
-    NUM_ROWS={NUM_ROWS}
-    NUM_COL={NUM_COL}
-    activeGame={activeGame}
-  />
-)
+const Game = () => {
+  const [gameStart, setGameStart] = useState(false)
+  const [NUM_ROWS, setNumRows] = useState(null)
+  const [NUM_COL, setNumCol] = useState(null)
+  const [createNewGrid, setCreateNewGrid] = useState(false)
+  const [randomize, setRandomize] = useState(false)
+  const [iteration, setIteration] = useState(0)
+  const [iterationSpeed, setIterationSpeed] = useState(200)
+
+  useEffect(() => {
+    setIteration(iteration + 1)
+  }, [gameStart])
+
+  return (
+    <>
+      <Navigation
+        NUM_ROWS={NUM_ROWS}
+        NUM_COL={NUM_COL}
+        createNewGrid={createNewGrid}
+        setCreateNewGrid={setCreateNewGrid}
+        setNumRows={setNumRows}
+        setNumCol={setNumCol}
+        setRandomize={setRandomize}
+        iterationSpeed={iterationSpeed}
+        setIterationSpeed={setIterationSpeed}
+        gameStart={gameStart}
+        setGameStart={setGameStart}
+      />
+      <Window
+        NUM_ROWS={NUM_ROWS}
+        NUM_COL={NUM_COL}
+        randomize={randomize}
+        createNewGrid={createNewGrid}
+        iteration={iteration}
+      />
+    </>
+  )
+}
 
 export default Game
