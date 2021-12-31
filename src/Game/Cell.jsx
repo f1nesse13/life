fcd727c0365fa6e8650b8ee9c6b4d72db0041d60
f1nesse13 <philly.teg@gmail.com>
2021-12-31 16:28:52 -0500

    add cell animations

--- src/Game/Cell.jsx
+++ src/Game/Cell.jsx
@@ -1,24 +1,49 @@
 import { useEffect, useRef } from 'react'
 import styles from './styles.module.scss'
 
-const Cell = ({ gridX, gridY, isActive }) => {
+const Cell = ({
+  gridX,
+  gridY,
+  isActive,
+  wasActive,
+  cellSize,
+  fadeIn,
+  fadeOut,
+  activeColor,
+  inactiveColor,
+  handleClickCell,
+}) => {
   const cell = useRef(`${gridY}${gridX}`)
-  useEffect(() => {
-    drawCell()
-  })
 
-  const drawCell = () => {
-    const element = cell.current
+  useEffect(() => {
+    const targetCell = cell.current
+    drawCell(targetCell)
+    animateCell(targetCell)
+  }, [isActive])
+
+  const animateCell = (targetCell) => {
+    if (isActive && !wasActive) {
+      targetCell.animate({ backgroundColor: [inactiveColor, activeColor] }, { duration: fadeIn })
+    }
+    if (wasActive && !isActive) {
+      targetCell.animate({ backgroundColor: [activeColor, inactiveColor] }, { duration: fadeOut })
+    }
+  }
+  const drawCell = (targetCell) => {
     if (isActive) {
-      element.classList.remove(styles.inactive)
-      element.classList.add(styles.active)
+      targetCell.classList.remove(styles.inactive)
+      targetCell.classList.add(styles.active)
     } else {
-      element.classList.remove(styles.active)
-      element.classList.add(styles.inactive)
+      targetCell.classList.remove(styles.active)
+      targetCell.classList.add(styles.inactive)
     }
   }
 
-  return <div ref={cell} className={`${styles.cell}`} />
+  return (
+    <div className={styles.cell} aria-hidden="true">
+      <div ref={cell} className={styles.inner_cell} style={{ width: cellSize, height: cellSize }} />
+    </div>
+  )
 }
 
 export default Cell
