c972b2053ff209d14074ebbdb628ee167411fc72
f1nesse13 <philly.teg@gmail.com>
2022-01-15 15:06:17 -0500

    random color helpers

--- src/helpers/randomValues.js
+++ src/helpers/randomValues.js
@@ -1,8 +1,6 @@
 import { isArray } from './types'
 
-const randomNumber = (upperLimit = 0) => {
-  Math.round(Math.random * upperLimit)
-}
+const randomNumber = (upperLimit = 0) => Math.round(Math.random() * upperLimit)
 
 export const randomPick = (valuesArray) => {
   if (isArray(valuesArray)) {
@@ -10,4 +8,17 @@ export const randomPick = (valuesArray) => {
     return valuesArray[index]
   }
   throw Error('Must pass an array of values. Use randomNumber for integers')
+}
+
+const generateRandomColor = (colors) => randomPick(colors)
+
+export const generateRandomColorPalette = (colors, numColors = 6) => {
+  let colorArr = []
+  while (colorArr.length < numColors) {
+    const newColor = generateRandomColor(colors)
+    if (!colorArr.includes(newColor)) {
+      colorArr = [...colorArr, newColor]
+    }
+  }
+  return colorArr
 }
